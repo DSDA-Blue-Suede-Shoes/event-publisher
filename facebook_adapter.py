2af0b1033cd3493477167780cced06b70f7f6afe
Casper Cromjongh <casper.cromjongh@hotmail.com>
2023-03-10 00:26:36 +0100

    Fix new event creation for FB.
    Xpath is lit.

--- facebook_adapter.py
+++ facebook_adapter.py
@@ -1,3 +1,4 @@
+import os
 import time
 
 from seleniumrequests import Firefox
@@ -105,25 +106,68 @@ class FacebookAdapter:
     def create_event(self, event_info: dict):
         self.driver.get("https://www.facebook.com/events/create/")
         pass
-        profile_svg = find_element_by_attribute(self.driver, By.TAG_NAME, "svg", "aria-label", ["Je profiel", "Your profile"])
+        profile_svg = self.driver.find_element(By.XPATH, '//*[name()="svg" and @aria-label="Je profiel"]')
         profile_svg.click()
-        profile_switch = find_element_by_attribute(self.driver, By.TAG_NAME, "div", "aria-label",
-                                                   ["profiel wisselen", "witch profile"])
+        profile_switch = self.driver.find_element(By.XPATH, '//div[@aria-label="Van profiel wisselen"]')
         profile_switch.click()
 
         time.sleep(2)
         WebDriverWait(self.driver, 10).until(
             expected_conditions.url_to_be("https://www.facebook.com/events/create/")
         )
+        time.sleep(0.5)
 
-        self.driver.get("https://www.facebook.com/events/create/")
-        omslagfoto = find_element_by_attribute(self.driver, By.TAG_NAME, "div", "aria-label", ["Omslagfoto toevoegen"])
-        filename = r"K:\Pictures\Ontwerpen\DSDA\Banner\Tango Café"
-        file = find_element_by_attribute(self.driver, By.TAG_NAME, "input", "type", ["file"])
+        filename = os.path.join(os.getcwd(), "event-image.jpg")
+        file = self.driver.find_element(By.XPATH, '//input[@type="file"]')
         file.send_keys(filename)
-        omslagfoto.click()
-        actions = ActionChains(self.driver)
-        actions.send_keys(Keys.TAB)
-        actions.send_keys(Keys.ENTER)
-        actions.perform()
-        pass
+
+        # Open end date and time
+        self.driver.find_element(By.XPATH, '//*[contains(text(), "Einddatum en -tijd")]').click()
+        event_name_field = self.driver.find_element(By.XPATH, '//label[@aria-label="Evenementnaam"]//input')
+        event_name_field.send_keys(event_info['name'])
+
+        event_start_date_field = self.driver.find_element(By.XPATH, '//label[@aria-label="Begindatum"]//input')
+        event_start_date_field.send_keys([Keys.BACK_SPACE] * 20 + [Keys.DELETE] * 20)
+        event_start_date_field.send_keys(event_info['start'].strftime("%d-%m-%Y"))
+
+        event_start_time_field = self.driver.find_element(By.XPATH, '//label[@aria-label="Starttijd"]//input')
+        event_start_time_field.click()
+        event_start_time_field.send_keys([Keys.BACK_SPACE] * 20 + [Keys.DELETE] * 20)
+        event_start_time_field.send_keys(event_info['start'].strftime("%H:%M"))
+
+        event_end_date_field = self.driver.find_element(By.XPATH, '//label[@aria-label="Einddatum"]//input')
+        event_end_date_field.click()
+        event_end_date_field.send_keys([Keys.BACK_SPACE] * 20 + [Keys.DELETE] * 20)
+        event_end_date_field.send_keys(event_info['end'].strftime("%d-%m-%Y"))
+
+        event_end_time_field = self.driver.find_element(By.XPATH, '//label[@aria-label="Eindtijd"]//input')
+        event_end_time_field.click()
+        event_end_time_field.send_keys([Keys.BACK_SPACE] * 20 + [Keys.DELETE] * 20)
+        event_end_time_field.send_keys(event_info['end'].strftime("%H:%M"))
+
+        # Click this is a personal event, not online
+        self.driver.find_element(By.XPATH, '//label[@aria-label="Is dit persoonlijk of virtueel?"]').click()
+        self.driver.find_element(By.XPATH, '//div[@role="option"][1]').click()  # First option
+
+        location_field = self.driver.find_element(By.XPATH, '//label[@aria-label="Locatie toevoegen"]//input')
+        location_field.send_keys(f"{event_info['venue']}, {event_info['address']}")
+
+        event_name_field.click()  # Make other fields visible again
+        details_field = self.driver.find_element(By.XPATH, '//label[@aria-label="Wat zijn de details?"]//textarea')
+        details_field.click()
+        details_field.send_keys(event_info['content-unicode'])
+
+        # Hit create event
+        self.driver.find_element(By.XPATH, '//label[@aria-label="Evenement maken"]').click()
+
+        WebDriverWait(self.driver, 10).until(
+            expected_conditions.url_changes("https://www.facebook.com/events/create/")
+        )
+
+        long_url = self.driver.current_url
+        print(f"Facebook: Created new event at {long_url}")
+        self.driver.find_element(By.XPATH, '//label[@aria-label="Delen"]').click()
+        short_url = self.driver.find_element(By.XPATH, '//span[contains(text(), "fb.me")]').text
+        print(f"          Short URL is {short_url}")
+
+        return long_url, short_url

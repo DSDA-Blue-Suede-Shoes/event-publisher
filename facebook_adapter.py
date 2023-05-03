0b458a4297f0c85aa933266bc0f6c81984035ae1
Casper Cromjongh <casper.cromjongh@hotmail.com>
2023-05-03 16:13:20 +0200

    Deal with Facebooks latest changes.
    Login screen change and event edit screen change.

--- facebook_adapter.py
+++ facebook_adapter.py
@@ -4,7 +4,7 @@ import time
 import requests
 from seleniumrequests import Firefox
 from selenium.webdriver.common.keys import Keys
-from selenium.common import TimeoutException
+from selenium.common import TimeoutException, NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
@@ -58,12 +58,13 @@ class FacebookAdapter(AdapterBase):
         if self.logged_in:
             return
         self.driver.get("https://www.facebook.com/")
-        essential_cookies_button = self.driver.find_element(By.XPATH, '//button[contains(text(), "Only allow essential cookies")]')
+        essential_cookies_button = self.driver.find_element(By.XPATH, '//button[@title="Decline optional cookies"]')
         login_button = self.driver.find_element(By.XPATH, '//button[@name="login"]')
         email_field = self.driver.find_element(By.ID, "email")
         password_field = self.driver.find_element(By.ID, "pass")
 
         essential_cookies_button.click()
+        time.sleep(0.3)
         email_field.send_keys(self.__username)
         password_field.send_keys(self.__password)
         login_button.click()
@@ -129,91 +130,23 @@ class FacebookAdapter(AdapterBase):
         return_events = api_response.json()['data']
         return return_events
 
-    @login_required
-    def update_event(self, event_info: dict, existing_event: dict):
-        edit_url = f"https://www.facebook.com/events/edit/{existing_event['id']}"
-        self.driver.get(edit_url)
-        time.sleep(0.5)
-
-        event_name_field = self.driver.find_element(By.XPATH, '//label[@aria-label="Evenementnaam"]//input')
-        event_name_field.clear()
-        event_name_field.send_keys(event_info['name'])
-
-        event_start_date_field = self.driver.find_element(By.XPATH, '//label[@aria-label="Begindatum"]//input')
-        event_start_date_field.send_keys([Keys.BACK_SPACE] * 20 + [Keys.DELETE] * 20)
-        event_start_date_field.send_keys(event_info['start'].strftime("%d-%m-%Y"))
-
-        event_start_time_field = self.driver.find_element(By.XPATH, '//label[@aria-label="Starttijd"]//input')
-        event_start_time_field.click()
-        event_start_time_field.send_keys([Keys.BACK_SPACE] * 20 + [Keys.DELETE] * 20)
-        event_start_time_field.send_keys(event_info['start'].strftime("%H:%M"))
-
-        event_end_date_field = self.driver.find_element(By.XPATH, '//label[@aria-label="Einddatum"]//input')
-        event_end_date_field.click()
-        event_end_date_field.send_keys([Keys.BACK_SPACE] * 20 + [Keys.DELETE] * 20)
-        event_end_date_field.send_keys(event_info['end'].strftime("%d-%m-%Y"))
-
-        event_end_time_field = self.driver.find_element(By.XPATH, '//label[@aria-label="Eindtijd"]//input')
-        event_end_time_field.click()
-        event_end_time_field.send_keys([Keys.BACK_SPACE] * 20 + [Keys.DELETE] * 20)
-        event_end_time_field.send_keys(event_info['end'].strftime("%H:%M"))
-
-        event_name_field.click()  # Make other fields visible again
-        details_field = self.driver.find_element(By.XPATH, '//label[@aria-label="Beschrijving"]//textarea')
-        details_field.clear()
-        details_field.click()
-        details_field.send_keys(event_info['content-unicode'])
-
-        # Click category
-        self.driver.find_element(By.XPATH, '//label[@aria-label="Categorie"]').click()
-        # Click dance
-        self.driver.find_element(By.XPATH, '//div[@role="option"]//span[contains(text(), "Dans")]').click()
-
-        # Hit next
-        self.driver.find_element(By.XPATH, '//div[@aria-label="Volgende"]').click()
-
-        location_field = self.driver.find_element(By.XPATH, '//label[@aria-label="Locatie"]//input')
-        location_field.clear()
-        location_field.send_keys(f"{event_info['venue']}, {event_info['address']}")
-
-        # Hit next
-        self.driver.find_element(By.XPATH, '//div[@aria-label="Volgende"]').click()
-
-        # Remove image
-        self.driver.find_element(By.XPATH, '//div[@aria-label="Afbeelding verwijderen"]').click()
-
-        # Put new image
-        filename = os.path.join(os.getcwd(), event_info['image_name'])
-        file = self.driver.find_element(By.XPATH, '//input[@type="file"]')
-        file.send_keys(filename)
-        time.sleep(1)
-
-        # Hit update
-        self.driver.find_element(By.XPATH, '//div[@aria-label="Bijwerken"]').click()
-
-        WebDriverWait(self.driver, 10).until(
-            expected_conditions.url_contains(f"https://www.facebook.com/events/{existing_event['id']}")
-        )
-
-        long_url, short_url = self.get_event_urls()
-        print(f"Facebook: Edited event at {long_url}")
-        print(f"          Short URL is {short_url}")
-        return long_url, short_url
-
-    @login_required
-    def create_event(self, event_info: dict):
-        self.driver.get("https://www.facebook.com/events/create/")
-        time.sleep(0.5)
-
+    def _fil_event_info(self, event_info: dict):
+        # Setting image
         filename = os.path.join(os.getcwd(), event_info['image_name'])
         file = self.driver.find_element(By.XPATH, '//input[@type="file"]')
         file.send_keys(filename)
 
-        # Open end date and time
-        self.driver.find_element(By.XPATH, '//*[contains(text(), "Einddatum en -tijd")]').click()
+        self.driver.implicitly_wait(0)
+        end_field_test = self.driver.find_elements(By.XPATH, '//label[@aria-label="Einddatum"]//input')
+        if len(end_field_test) == 0:
+            # Open end date and time
+            self.driver.find_element(By.XPATH, '//*[contains(text(), "Einddatum en -tijd")]').click()
+        self.driver.implicitly_wait(5)
+
         event_name_field = self.driver.find_element(By.XPATH, '//label[@aria-label="Evenementnaam"]//input')
         event_name_field.send_keys(event_info['name'])
 
+        # Date and time
         event_start_date_field = self.driver.find_element(By.XPATH, '//label[@aria-label="Begindatum"]//input')
         event_start_date_field.send_keys([Keys.BACK_SPACE] * 20 + [Keys.DELETE] * 20)
         event_start_date_field.send_keys(event_info['start'].strftime("%d-%m-%Y"))
@@ -237,13 +170,44 @@ class FacebookAdapter(AdapterBase):
         self.driver.find_element(By.XPATH, '//label[@aria-label="Is dit persoonlijk of virtueel?"]').click()
         self.driver.find_element(By.XPATH, '//div[@role="option"][1]').click()  # First option
 
+        # Location
         location_field = self.driver.find_element(By.XPATH, '//label[@aria-label="Locatie toevoegen"]//input')
+        location_field.clear()
         location_field.send_keys(f"{event_info['venue']}, {event_info['address']}")
 
         event_name_field.click()  # Make other fields visible again
+        # Event details (description)
         details_field = self.driver.find_element(By.XPATH, '//label[@aria-label="Wat zijn de details?"]//textarea')
         details_field.click()
+        details_field.clear()
         details_field.send_keys(event_info['content-unicode'])
+
+    @login_required
+    def update_event(self, event_info: dict, existing_event: dict):
+        edit_url = f"https://www.facebook.com/events/edit/{existing_event['id']}"
+        self.driver.get(edit_url)
+        time.sleep(0.5)
+
+        self._fil_event_info(event_info)
+
+        # Hit update
+        self.driver.find_element(By.XPATH, '//div[@aria-label="Opslaan"]').click()
+
+        WebDriverWait(self.driver, 10).until(
+            expected_conditions.url_contains(f"https://www.facebook.com/events/{existing_event['id']}")
+        )
+
+        long_url, short_url = self.get_event_urls()
+        print(f"Facebook: Edited event at {long_url}")
+        print(f"          Short URL is {short_url}")
+        return long_url, short_url
+
+    @login_required
+    def create_event(self, event_info: dict):
+        self.driver.get("https://www.facebook.com/events/create/")
+        time.sleep(0.5)
+
+        self._fil_event_info(event_info)
 
         # Hit create event
         self.driver.find_element(By.XPATH, '//div[@aria-label="Evenement maken"]').click()
@@ -261,7 +225,7 @@ class FacebookAdapter(AdapterBase):
     @login_required
     def get_event_urls(self) -> tuple[str, str]:
         long_url = self.driver.current_url.split('?')[0]
-        self.driver.find_element(By.XPATH, '//div[@aria-label="Delen"]').click()
-        time.sleep(0.1)
-        short_url = self.driver.find_element(By.XPATH, '//span[contains(text(), "fb.me")]').text
+        # self.driver.find_element(By.XPATH, '//div[@aria-label="Delen"]').click()
+        # time.sleep(0.1)
+        short_url = ""  # self.driver.find_element(By.XPATH, '//span[contains(text(), "fb.me")]').text
         return long_url, short_url
